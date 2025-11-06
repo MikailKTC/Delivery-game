2eb0d8f6144687180c93ea0158bc200db1eb3feb
JoeKhaz <joe.khazzaka@outlook.com>
2025-11-06 08:46:14 -0500

    Update MainJavaFX.java

--- TP2-Camelot-velo/src/main/java/ca/qc/bdeb/sim/tp2camelotvelo/MainJavaFX.java
+++ TP2-Camelot-velo/src/main/java/ca/qc/bdeb/sim/tp2camelotvelo/MainJavaFX.java
@@ -11,10 +11,6 @@ public class MainJavaFX extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         //J'ai modifié ici
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
     }
 
     public static void main(String[] args) {
