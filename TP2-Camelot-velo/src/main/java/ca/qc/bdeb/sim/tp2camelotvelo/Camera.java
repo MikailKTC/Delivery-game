abe1d1152e3d3fdb86d30ca8f81607a37e2a692a
JoeKhaz <joe.khazzaka@outlook.com>
2025-11-07 19:37:18 -0500

    Update Camera.java

--- TP2-Camelot-velo/src/main/java/ca/qc/bdeb/sim/tp2camelotvelo/Camera.java
+++ TP2-Camelot-velo/src/main/java/ca/qc/bdeb/sim/tp2camelotvelo/Camera.java
@@ -6,7 +6,21 @@ public class Camera {
 
     private Point2D positionCamera;
 
+
+    public Camera() {
+        positionCamera = new Point2D(0, 0);
+    }
+
     public Point2D coordoEcran(Point2D positionMonde) {
         return positionMonde.subtract(positionCamera);
+    }
+
+    public void suivreCamelot(Camelot camelot) {
+        double nouveauX = camelot.getPosition().getX() - 900 * 0.2;
+        positionCamera = new Point2D(nouveauX, 0);
+    }
+
+    public Point2D getPositionCamera() {
+        return positionCamera;
     }
 }
