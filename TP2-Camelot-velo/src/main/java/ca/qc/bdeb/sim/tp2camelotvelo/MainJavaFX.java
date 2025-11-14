4ce824c137f0cfcd84f025501a73d5fea4d2e818
JoeKhaz <joe.khazzaka@outlook.com>
2025-11-14 08:37:56 -0500

    Icone de l'application

--- TP2-Camelot-velo/src/main/java/ca/qc/bdeb/sim/tp2camelotvelo/MainJavaFX.java
+++ TP2-Camelot-velo/src/main/java/ca/qc/bdeb/sim/tp2camelotvelo/MainJavaFX.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -50,6 +51,8 @@ public class MainJavaFX extends Application {
 
         stage.setResizable(false);
 
+        Image icon = new Image("journal.png");
+        stage.getIcons().add(icon);
         stage.show();
     }
 
